86d5d74605f5e4a7c3780acdd9dea0a28b14f526
faryozbekTFK <t.faryozbek1311@gmail.com>
2022-08-16 16:47:33 +0500

    Bugs update | 16.08 | 16:47

--- src/store/actions/actionCreaters.js
+++ src/store/actions/actionCreaters.js
@@ -1,29 +1,47 @@
-import { GET_CATEGORIES_PROCCESS, GET_CATEGORIES_SUCCESS, GET_QUESTIONS_PROCCESS, GET_QUESTIONS_SUCCESS, SET_CURRENT_CATEGORIES, SET_QUESTION_PAGINATION } from "./actionTypes";
+import {
+  GET_CATEGORIES_PROCCESS,
+  GET_CATEGORIES_SUCCESS,
+  GET_QUESTIONS_PROCCESS,
+  GET_QUESTIONS_SUCCESS,
+  SET_QUESTION_PAGINATION,
+  GET_USERS_PROCESS,
+  GET_USERS_SUCCESS,
+  SET_CURRENT_CATEGORIES,
+} from './actionTypes';
 
 export const getCategoriesProccess = () => ({
-    type: GET_CATEGORIES_PROCCESS
+  type: GET_CATEGORIES_PROCCESS,
 });
 
-export const getCategoriesSuccess = (categories) => ({
-    type: GET_CATEGORIES_SUCCESS,
-    payload: categories,
+export const getCategoriesSuccess = categories => ({
+  type: GET_CATEGORIES_SUCCESS,
+  payload: categories,
 });
 
-export const setCurrentCategories = (category) => ({
-    type: SET_CURRENT_CATEGORIES,
-    payload: category
+export const setCurrentCategories = currCategory => ({
+  type: SET_CURRENT_CATEGORIES,
+  payload: currCategory
 })
 
 export const getQuestionsProccess = () => ({
-    type: GET_QUESTIONS_PROCCESS
-})
+  type: GET_QUESTIONS_PROCCESS,
+});
 
-export const getQuestionsSuccess = (questions) => ({
-    type: GET_QUESTIONS_SUCCESS,
-    payload: questions,
-})
+export const getQuestionsSuccess = questions => ({
+  type: GET_QUESTIONS_SUCCESS,
+  payload: questions,
+});
 
-export const setQuestionsPagination = (pagination) => ({
-    type: SET_QUESTION_PAGINATION,
-    payload: pagination
-})
+export const setQuestionsPagination = pagination => ({
+  type: SET_QUESTION_PAGINATION,
+  payload: pagination,
+});
+
+export const getUsersProcess = () => ({
+  type: GET_USERS_PROCESS,
+});
+
+export const getUsersSuccess = users => ({
+  type: GET_USERS_SUCCESS,
+  payload: users,
+});
