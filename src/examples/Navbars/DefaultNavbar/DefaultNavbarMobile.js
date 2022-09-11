4828d6cf3ee04e835504545a19de81dfb37c923e
faryozbekTFK <t.faryozbek1311@gmail.com>
2022-09-11 17:38:14 +0500

    Some bugs update

--- src/examples/Navbars/DefaultNavbar/DefaultNavbarMobile.js
+++ src/examples/Navbars/DefaultNavbar/DefaultNavbarMobile.js
@@ -1,14 +1,14 @@
 // prop-types is a library for typechecking of props.
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 // @mui material components
-import Menu from "@mui/material/Menu";
+import Menu from '@mui/material/Menu';
 
 // Material Dashboard 2 React components
-import MDBox from "components/MDBox";
+import MDBox from 'components/MDBox';
 
 // Material Dashboard 2 React example components
-import DefaultNavbarLink from "examples/Navbars/DefaultNavbar/DefaultNavbarLink";
+import DefaultNavbarLink from 'examples/Navbars/DefaultNavbar/DefaultNavbarLink';
 
 function DefaultNavbarMobile({ open, close }) {
   const { width } = open && open.getBoundingClientRect();
@@ -17,12 +17,12 @@ function DefaultNavbarMobile({ open, close }) {
     <Menu
       getContentAnchorEl={null}
       anchorOrigin={{
-        vertical: "bottom",
-        horizontal: "center",
+        vertical: 'bottom',
+        horizontal: 'center',
       }}
       transformOrigin={{
-        vertical: "top",
-        horizontal: "center",
+        vertical: 'top',
+        horizontal: 'center',
       }}
       anchorEl={open}
       open={Boolean(open)}
@@ -30,10 +30,10 @@ function DefaultNavbarMobile({ open, close }) {
       MenuListProps={{ style: { width: `calc(${width}px - 4rem)` } }}
     >
       <MDBox px={0.5}>
-        <DefaultNavbarLink icon="donut_large" name="dashboard" route="/dashboard" />
-        <DefaultNavbarLink icon="person" name="profile" route="/profile" />
-        <DefaultNavbarLink icon="account_circle" name="sign up" route="/authentication/sign-up" />
-        <DefaultNavbarLink icon="key" name="sign in" route="/authentication/sign-in" />
+        <DefaultNavbarLink icon='donut_large' name='dashboard' route='/dashboard' />
+        <DefaultNavbarLink icon='person' name='profile' route='/profile' />
+        <DefaultNavbarLink icon='account_circle' name='sign up' route='/authentication/sign-up' />
+        <DefaultNavbarLink icon='key' name='sign in' route='/authentication/sign-in' />
       </MDBox>
     </Menu>
   );
--- src/examples/Navbars/DefaultNavbar/index.js
+++ src/examples/Navbars/DefaultNavbar/index.js
@@ -1,29 +1,29 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
 // react-router components
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 // prop-types is a library for typechecking of props.
-import PropTypes from "prop-types";
+import PropTypes from 'prop-types';
 
 // @mui material components
-import Container from "@mui/material/Container";
-import Icon from "@mui/material/Icon";
+import Container from '@mui/material/Container';
+import Icon from '@mui/material/Icon';
 
 // Material Dashboard 2 React components
-import MDBox from "components/MDBox";
-import MDTypography from "components/MDTypography";
-import MDButton from "components/MDButton";
+import MDBox from 'components/MDBox';
+import MDTypography from 'components/MDTypography';
+import MDButton from 'components/MDButton';
 
 // Material Dashboard 2 React example components
-import DefaultNavbarLink from "examples/Navbars/DefaultNavbar/DefaultNavbarLink";
-import DefaultNavbarMobile from "examples/Navbars/DefaultNavbar/DefaultNavbarMobile";
+import DefaultNavbarLink from 'examples/Navbars/DefaultNavbar/DefaultNavbarLink';
+import DefaultNavbarMobile from 'examples/Navbars/DefaultNavbar/DefaultNavbarMobile';
 
 // Material Dashboard 2 React base styles
-import breakpoints from "assets/theme/base/breakpoints";
+import breakpoints from 'assets/theme/base/breakpoints';
 
 // Material Dashboard 2 React context
-import { useMaterialUIController } from "context";
+import { useMaterialUIController } from 'context';
 
 function DefaultNavbar({ transparent, light, action }) {
   const [controller] = useMaterialUIController();
@@ -51,13 +51,13 @@ function DefaultNavbar({ transparent, light, action }) {
      The event listener that's calling the displayMobileNavbar function when 
      resizing the window.
     */
-    window.addEventListener("resize", displayMobileNavbar);
+    window.addEventListener('resize', displayMobileNavbar);
 
     // Call the displayMobileNavbar function to set the state with the initial value.
     displayMobileNavbar();
 
     // Remove event listener on cleanup
-    return () => window.removeEventListener("resize", displayMobileNavbar);
+    return () => window.removeEventListener('resize', displayMobileNavbar);
   }, []);
 
   return (
@@ -67,14 +67,14 @@ function DefaultNavbar({ transparent, light, action }) {
         px={{ xs: 4, sm: transparent ? 2 : 3, lg: transparent ? 0 : 2 }}
         my={3}
         mx={3}
-        width="calc(100% - 48px)"
-        borderRadius="lg"
-        shadow={transparent ? "none" : "md"}
-        color={light ? "white" : "dark"}
-        display="flex"
-        justifyContent="space-between"
-        alignItems="center"
-        position="absolute"
+        width='calc(100% - 48px)'
+        borderRadius='lg'
+        shadow={transparent ? 'none' : 'md'}
+        color={light ? 'white' : 'dark'}
+        display='flex'
+        justifyContent='space-between'
+        alignItems='center'
+        position='absolute'
         left={0}
         zIndex={3}
         sx={({
@@ -84,59 +84,59 @@ function DefaultNavbar({ transparent, light, action }) {
           backgroundColor: transparent
             ? transparentColor.main
             : rgba(darkMode ? background.sidenav : white.main, 0.8),
-          backdropFilter: transparent ? "none" : `saturate(200%) blur(30px)`,
+          backdropFilter: transparent ? 'none' : `saturate(200%) blur(30px)`,
         })}
       >
         <MDBox
           component={Link}
-          to="/"
+          to='/'
           py={transparent ? 1.5 : 0.75}
           lineHeight={1}
           pl={{ xs: 0, lg: 1 }}
         >
-          <MDTypography variant="button" fontWeight="bold" color={light ? "white" : "dark"}>
+          <MDTypography variant='button' fontWeight='bold' color={light ? 'white' : 'dark'}>
             Material Dashboard 2
           </MDTypography>
         </MDBox>
-        <MDBox color="inherit" display={{ xs: "none", lg: "flex" }} m={0} p={0}>
-          <DefaultNavbarLink icon="donut_large" name="dashboard" route="/dashboard" light={light} />
-          <DefaultNavbarLink icon="person" name="profile" route="/profile" light={light} />
+        <MDBox color='inherit' display={{ xs: 'none', lg: 'flex' }} m={0} p={0}>
+          <DefaultNavbarLink icon='donut_large' name='dashboard' route='/dashboard' light={light} />
+          <DefaultNavbarLink icon='person' name='profile' route='/profile' light={light} />
           <DefaultNavbarLink
-            icon="account_circle"
-            name="sign up"
-            route="/authentication/sign-up"
+            icon='account_circle'
+            name='sign up'
+            route='/authentication/sign-up'
             light={light}
           />
           <DefaultNavbarLink
-            icon="key"
-            name="sign in"
-            route="/authentication/sign-in"
+            icon='key'
+            name='sign in'
+            route='/authentication/sign-in'
             light={light}
           />
         </MDBox>
         {action &&
-          (action.type === "internal" ? (
-            <MDBox display={{ xs: "none", lg: "inline-block" }}>
+          (action.type === 'internal' ? (
+            <MDBox display={{ xs: 'none', lg: 'inline-block' }}>
               <MDButton
                 component={Link}
                 to={action.route}
-                variant="gradient"
-                color={action.color ? action.color : "info"}
-                size="small"
+                variant='gradient'
+                color={action.color ? action.color : 'info'}
+                size='small'
               >
                 {action.label}
               </MDButton>
             </MDBox>
           ) : (
-            <MDBox display={{ xs: "none", lg: "inline-block" }}>
+            <MDBox display={{ xs: 'none', lg: 'inline-block' }}>
               <MDButton
-                component="a"
+                component='a'
                 href={action.route}
-                target="_blank"
-                rel="noreferrer"
-                variant="gradient"
-                color={action.color ? action.color : "info"}
-                size="small"
+                target='_blank'
+                rel='noreferrer'
+                variant='gradient'
+                color={action.color ? action.color : 'info'}
+                size='small'
                 sx={{ mt: -0.3 }}
               >
                 {action.label}
@@ -144,15 +144,15 @@ function DefaultNavbar({ transparent, light, action }) {
             </MDBox>
           ))}
         <MDBox
-          display={{ xs: "inline-block", lg: "none" }}
+          display={{ xs: 'inline-block', lg: 'none' }}
           lineHeight={0}
           py={1.5}
           pl={1.5}
-          color="inherit"
-          sx={{ cursor: "pointer" }}
+          color='inherit'
+          sx={{ cursor: 'pointer' }}
           onClick={openMobileNavbar}
         >
-          <Icon fontSize="default">{mobileNavbar ? "close" : "menu"}</Icon>
+          <Icon fontSize='default'>{mobileNavbar ? 'close' : 'menu'}</Icon>
         </MDBox>
       </MDBox>
       {mobileView && <DefaultNavbarMobile open={mobileNavbar} close={closeMobileNavbar} />}
@@ -174,17 +174,17 @@ DefaultNavbar.propTypes = {
   action: PropTypes.oneOfType([
     PropTypes.bool,
     PropTypes.shape({
-      type: PropTypes.oneOf(["external", "internal"]).isRequired,
+      type: PropTypes.oneOf(['external', 'internal']).isRequired,
       route: PropTypes.string.isRequired,
       color: PropTypes.oneOf([
-        "primary",
-        "secondary",
-        "info",
-        "success",
-        "warning",
-        "error",
-        "dark",
-        "light",
+        'primary',
+        'secondary',
+        'info',
+        'success',
+        'warning',
+        'error',
+        'dark',
+        'light',
       ]),
       label: PropTypes.string.isRequired,
     }),
